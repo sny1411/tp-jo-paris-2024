06bde577049193d329fb978908f3b5364ec32652
matteo_humez <matteo_humez@ens.univ-artois.fr>
2024-01-13 17:44:48 +0100

    :bug: oubli de footer et header pour le layout

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,4 @@
+<p>Copyright &copy; {{ date('Y')}} <a href="http://www.iut-lens.univ-artois.fr/" target="_blank">
+        <strong>IUT Lens - département Info.</strong></a></p>
+<p>Humez Mattéo</p>
+
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,5 @@
+<button><a href="{{route('accueil')}}">🏛 Accueil</a></button>
+<button><a href="{{route('apropos')}}">ℹ️ A Propos</a></button>
+<button><a href="{{route('contact')}}">📞 Contacts</a></button>
+<button><a href="{{route('sports.index')}}">📜 Sports</a></button>
+<button><a href="">🔒 Connexion </a></button>
