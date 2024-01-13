ad13c292388c5ea330e0c7b932f4b17cd5963c95
matteo_humez <matteo_humez@ens.univ-artois.fr>
2024-01-13 22:44:01 +0100

    :bug: oublie d'un bouton pour acceder à la création d'un sport

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,6 +2,7 @@
 <a href="{{route('apropos')}}"><button>ℹ️ A Propos</button></a>
 <a href="{{route('contact')}}"><button>📞 Contacts</button></a>
 <a href="{{route('sports.index')}}"><button>📜 Sports</button></a>
+<a href="{{route('sports.create')}}"><button>➕ Ajouter un sport</button></a>
 <div class="menu toRight">
     <a href=""><button>🔒 Connexion</button></a>
 </div>
