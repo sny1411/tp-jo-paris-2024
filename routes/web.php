5da73895ea490e11580952fd8bd45650feb9bc30
matteo_humez <matteo_humez@ens.univ-artois.fr>
2024-01-13 15:22:02 +0100

    :sparkles: Question 2.2.3 du tp introduction Laravel - Ajout de la route '/sports' pour afficher la liste des sports

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SportController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sports', [SportController::class, 'index'])->name('sports.index');
