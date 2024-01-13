5cc5e7ae0a041214ebd8832a8a52d8138076dabe
matteo_humez <matteo_humez@ens.univ-artois.fr>
2024-01-13 14:57:52 +0100

    :sparkles: Question 1.2.4 du tp introduction Laravel - Génération des données aléatoires dans la table sport

---
+++ database/factories/SportFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Sport;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Sport>
+ */
+class SportFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nom' => $this->faker->word,
+            'description' => $this->faker->paragraph,
+            'annee_ajout' => $this->faker->biasedNumberBetween(1900,2023),
+            'nb_disciplines' => $this->faker->randomDigit(),
+            'nb_epreuves' => $this->faker->randomDigit(),
+            'date_debut' => $this->faker->dateTimeInInterval,
+            'date_fin' => $this->faker->dateTimeInInterval
+        ];
+    }
+}
