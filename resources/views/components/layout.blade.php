a767aa0874e53d79adda0d814fd75cef31b46afb
matteo_humez <matteo_humez@ens.univ-artois.fr>
2024-01-13 17:41:49 +0100

    :sparkles: Création d'un composant layout

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang={{ app()->getLocale() }}>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{$titre ?? "Application Laravel"}}</title>
+</head>
+<body>
+<menu>
+    <x-header></x-header>
+</menu>
+<main>
+    {{$slot}}
+</main>
+<footer>
+    <x-footer></x-footer>
+</footer>
+</body>
+</html>
