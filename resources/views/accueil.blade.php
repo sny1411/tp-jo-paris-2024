bea3c1050ab131275818a0ddb2476361ab01ba2b
matteo_humez <matteo_humez@ens.univ-artois.fr>
2024-01-13 17:43:51 +0100

    :sparkles: Question 2.1 du tp routage crud Laravel - Création des vues accueil, apropos et contact

---
+++ resources/views/accueil.blade.php
@@ -0,0 +1,4 @@
+<x-layout :titre="$titre">
+    <h3>Accueil</h3>
+    <p>Bienvenue sur tp-jo-2024 !</p>
+</x-layout>
---
+++ resources/views/apropos.blade.php
@@ -0,0 +1,4 @@
+<x-layout :titre="$titre">
+        <h1>A Propos</h1>
+        <h2>Gestion des sports</h2>
+</x-layout>
